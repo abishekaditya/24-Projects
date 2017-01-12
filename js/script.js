c1828f95fb4d24528a68fcc682d22d2e13ecfac5
Abishek Aditya <abishekaditya@outlook.com>
2017-01-12 19:40:26 +0530

    Colorize using binding

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-function colorize() {
-    var colors = ["#FF6164","#A4E9FF","#B8F038", "#94D8F7", "#47EF85", "#FCD164","#9d84ff"];
-    var i = 0;
-    $('.carousel-item').each(function () {
-        i = (i+1)%colors.length;
-        $(this).css('background-color',colors[i]);
-    })
-}
-
 $(window).load((function () {
     $url = 'https://api.github.com/users/abishekaditya/repos';
     $.getJSON ($url, function ( response ) {
@@ -20,9 +11,9 @@ $(window).load((function () {
                 $('.carousel-inner').append('<div class="carousel-item"> ' + '<div class="num">' + i + '<br/></div><div class="parent row"><div class="col-sm-10">'
                     + repos.name + '</div><div class="col-sm-2"><a href='+ repos.html_url +'><button type="button" class="btn child">Repository</button></a></div></div>' +
                     '<br/><div class="desc"><br/>' +
-                    repos.description
+                    repos.description.replace('Project :','')
                     +'</div></div>');
-                colorize();
+                $('.carousel').css('background-color','#A4E9FF')
                 if(i == 1) {
                     $('.carousel-item').addClass('active');
                 }
@@ -31,3 +22,8 @@ $(window).load((function () {
         });
     });
 }));
+
+$('.carousel').bind('slide.bs.carousel', function () {
+    var colors = ["#FF6164","#A4E9FF","#B8F038", "#94D8F7", "#47EF85", "#FCD164","#9d84ff"]; 
+    $('.carousel').css('background-color', colors[(parseInt($('.active > .num').html(),10)+1) % colors.length])
+});
